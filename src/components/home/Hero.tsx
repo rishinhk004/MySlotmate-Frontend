4aee166869b54ccaa07ca3465688900a081f3d0a
Barnil Sarma <barnilsarma@gmail.com>
2026-02-28 17:43:10 +0530

    hero section desktop responsiveness left

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -10,6 +10,13 @@ const Hero: React.FC = () => {
                     <button className="w-[45%] h-[1.2rem] bg-[#0094CA] text-[#ffffff] text-xs rounded-md mt-2">Book Time</button>
                     <button className="w-[45%] h-[1.2rem] bg-[#ffffff] text-[#231F20] border border-[#BBBBBB] rounded-md mt-2 text-xs">List Time</button>
                 </div>
+                <div className="flex flex-row justify-center items-center md:justify-start mt-2">
+                    <div className="h-[1.5rem] w-[1.5rem] rounded-full bg-[#EEEEEE] border border-[#ffffff] z-0"><img className="rounded-full" src="/assets/home/profile1.png"/></div>
+                    <div className="h-[1.5rem] w-[1.5rem] rounded-full bg-[#EEEEEE] border border-[#ffffff] -translate-x-2 z-10"><img className="rounded-full" src="/assets/home/profile2.png"/></div>
+                    <div className="h-[1.5rem] w-[1.5rem] rounded-full bg-[#EEEEEE] border border-[#ffffff] -translate-x-4 z-20"><img className="rounded-full" src="/assets/home/profile3.png"/></div>
+                    <div className="h-[1.5rem] w-[1.5rem] rounded-full bg-[#EEEEEE] border border-[#ffffff] -translate-x-6 z-30 text-[#000000] text-[0.5rem] text-center p-1 flex items-center justify-center"><span>+10K</span></div>
+                    <p className="text-[0.5rem] text-[#8E8E8E] ml-2">Used by interesting people</p>
+                </div>
             </div>
             <div className='flex flex-col items-center justify-center'>
                 <HeroCard
