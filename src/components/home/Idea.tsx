0d040ef05481a70945114e81a6e22a3a7f0f1b57
Barnil Sarma <barnilsarma@gmail.com>
2026-03-20 14:52:13 +0530

    some change

--- src/components/home/Idea.tsx
+++ src/components/home/Idea.tsx
@@ -12,7 +12,7 @@ const Idea=()=>{
                     <p className="text-[#6B7280]">Our community is vetted to ensure safety and authenticity for every experience</p>
                 </div>
                 <div className="flex flex-col items-center justify-center">
-                    <div className="flex items-center justify-center h-[4rem] w-[4rem] bg-[#0094CA1A] rounded-full p-2">{/* eslint-disable-next-line @next/next/no-img-element */}<img src="/assets/home/icon.svg" alt="Right Fit" /></div>   
+                    <div className="flex items-center justify-center h-[4rem] w-[4rem] bg-[#0094CA1A] rounded-full p-2">{/* eslint-disable-next-line @next/next/no-img-element */}<img src="/assets/home/Icon.svg" alt="Right Fit" /></div>   
                     <h1 className="text-lg font-semibold">Right Fit</h1>
                     <p className="text-[#6B7280]">Don&apos;t just book a slot, book a moment. Find what feels right for you today.</p>
                 </div>
