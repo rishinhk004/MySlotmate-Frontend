7c89a6047fd111aba9f3f2c1840ef26c7063e72d
Trithanka Baruah <baruahtrithanka@gmail.com>
2026-05-21 22:13:46 +0530

    seo redirect

--- next.config.js
+++ next.config.js
@@ -9,6 +9,45 @@ const config = {
   images: {
     domains: ["myslotmate.s3.us-east-2.amazonaws.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/docs/trust-and-safety/community-guidelines",
+        destination: "/support/policies",
+        statusCode: 301,
+      },
+      {
+        source: "/docs/trust-and-safety/faqs",
+        destination: "/support",
+        statusCode: 301,
+      },
+      {
+        source: "/docs/trust-and-safety/security-practices",
+        destination: "/support/policies",
+        statusCode: 301,
+      },
+      {
+        source: "/docs/company/contact-us",
+        destination: "/support",
+        statusCode: 301,
+      },
+      {
+        source: "/docs/legal/accessibility-statement",
+        destination: "/support/policies",
+        statusCode: 301,
+      },
+      {
+        source: "/docs/legal/age-restriction-notice",
+        destination: "/support/policies",
+        statusCode: 301,
+      },
+      {
+        source: "/docs/legal/privacy-policy-and-data-protection",
+        destination: "/support/policies",
+        statusCode: 301,
+      },
+    ];
+  },
 };
 
 export default config;
